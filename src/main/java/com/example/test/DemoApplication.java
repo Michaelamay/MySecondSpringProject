77c4d199a5e0c1d45e65509d73417b33dfb60b1b
Michaelamay <michaelamay1@yahoo.com>
2026-02-17 11:40:59 -0500

    Beans and Dependency Injection

--- src/main/java/com/example/test/DemoApplication.java
+++ src/main/java/com/example/test/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -13,6 +14,11 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("Hello World from MySecondSpringProject");
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
