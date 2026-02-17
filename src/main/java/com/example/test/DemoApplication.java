f06d447199de714b16f416a79be7324b62af06a4
Michaelamay <michaelamay1@yahoo.com>
2026-02-17 11:50:20 -0500

    true default point

--- src/main/java/com/example/test/DemoApplication.java
+++ src/main/java/com/example/test/DemoApplication.java
@@ -1,24 +1,15 @@
 package com.example.test;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DemoApplication {
-	
-@Autowired RestTemplate restTemplate;	
 
 	public static void main(String[] args) {
-		System.out.println("Hello World, I am the starting point!");
+		System.out.println("Hello World, I am the true starting point!");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	
-	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
-	}
 
 }
