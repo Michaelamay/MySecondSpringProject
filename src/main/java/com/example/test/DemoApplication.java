7f714c0e5082ec0d29378958e6250c5b9a2dd5c2
Michaelamay <michaelamay1@yahoo.com>
2026-02-17 11:33:19 -0500

    2nd commit

--- src/main/java/com/example/test/DemoApplication.java
+++ src/main/java/com/example/test/DemoApplication.java
@@ -1,10 +1,14 @@
 package com.example.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DemoApplication {
+	
+@Autowired RestTemplate restTemplate;	
 
 	public static void main(String[] args) {
 		System.out.println("Hello World from MySecondSpringProject");
