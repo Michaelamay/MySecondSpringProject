e0d738556897a1a1c19242d4868c22497894206c
Michaelamay <michaelamay1@yahoo.com>
2026-02-17 11:48:46 -0500

    default commit

--- src/main/java/com/example/test/DemoApplication.java
+++ src/main/java/com/example/test/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 @Autowired RestTemplate restTemplate;	
 
 	public static void main(String[] args) {
-		System.out.println("Hello World from MySecondSpringProject");
+		System.out.println("Hello World, I am the starting point!");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	
